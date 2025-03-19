7ba35ccfc470a9804178f42879bb343495c48537
Gurnav224 <chaudharyg856@gmail.com>
2025-03-19 20:50:32 +0530

    feat: project, team, task reducer to the store

--- src/app/store.jsx
+++ src/app/store.jsx
@@ -1,9 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { authSlice } from "../features/auth/authSlice";
+import { projectSlice } from "../features/projects/projectSlice";
+import { teamSlice } from "../features/team/teamSlice";
+import { taskSlice } from "../features/tasks/taskSlice";
 
 const store = configureStore({
     reducer:{
-        auth:authSlice.reducer
+        auth:authSlice.reducer,
+        project:projectSlice.reducer,
+        team:teamSlice.reducer,
+        task:taskSlice.reducer
     }
 })
 
