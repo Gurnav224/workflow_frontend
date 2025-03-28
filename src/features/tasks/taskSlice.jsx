06cbd26982560b12f2d2521cdd336db0fac90818
Gurnav224 <chaudharyg856@gmail.com>
2025-03-28 11:42:15 +0530

    feat: add task update functionality

--- src/features/tasks/taskSlice.jsx
+++ src/features/tasks/taskSlice.jsx
@@ -2,7 +2,7 @@ import {createSlice, createAsyncThunk} from "@reduxjs/toolkit";
 import axios from "axios";
 
 
-const apiUrl = "http://localhost:8888";
+const apiUrl = `${import.meta.env.VITE_API_URL}`;
 
 export const fetchTags = createAsyncThunk("task/fetchTags", async (token) => {
     const { data } = await axios.get(`${apiUrl}/tags/all`,{
@@ -32,12 +32,31 @@ export const addTaskAsync = createAsyncThunk("task/addTaskAsync", async (task, {
 })
 
 
+export const fetchTaskById = createAsyncThunk("task/fetchTaskById", async (taskId , {rejectWithValue}) => {
+    try {
+        const {data} = await axios.get(`${apiUrl}/tasks/${taskId}`);
+        return data;
+    } catch (error) {
+        return rejectWithValue(error.response.data.error || 'failed to get  task by id')
+    }
+})
+
+export const updateTaskById = createAsyncThunk("task/updateTaskById", async (taskId , {rejectWithValue}) => {
+    try {
+        const {data} = await axios.put(`${apiUrl}/tasks/${taskId}`);
+        return data;
+    } catch (error) {
+            return rejectWithValue(error.response.data.error)
+    }
+})
+
 export const taskSlice = createSlice({
     name:"task",
     initialState:{
         status:"idle",
         error:null,
         tasks:[],
+        task:{},
         tags:[]
     },
     reducers:{
@@ -74,10 +93,34 @@ export const taskSlice = createSlice({
             builder.addCase(addTaskAsync.fulfilled, (state, action) => {
                 state.status = 'success';
                 state.tasks = [...state.tasks, action.payload]
+                state.status = ''
             })
             builder.addCase(addTaskAsync.rejected, (state, action) => {
                 state.status = 'failed';
                 state.error = action.payload
+            })
+            builder.addCase(fetchTaskById.pending, (state) => {
+                state.status = 'loading'
+            })
+            builder.addCase(fetchTaskById.fulfilled, (state, action) => {
+                state.status = 'success';
+                state.task = action.payload
+                state.error = ''
+            })
+            builder.addCase(fetchTaskById.rejected, (state, action) => {
+                state.status = 'failed';
+                state.error = action.payload 
+            })
+            builder.addCase(updateTaskById.pending, (state) => {
+                state.status = 'loading'
+            })
+            builder.addCase(updateTaskById.fulfilled, (state, action) => {
+                state.status = 'success';
+                state.task = action.payload;
+            })
+            builder.addCase(updateTaskById.rejected, (state) => {
+                state.status = 'failed';
+                state.error = 'failed to update the task '
             })
     }
 })
